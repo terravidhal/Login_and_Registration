4b842944a0e8e050df46867baac1ef830ff4ef19
terravidhal <infiniterraetincelle@gmail.com>
2023-09-19 13:01:00 +0100

    login_and_registration

---
+++ flask_app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask 
+app = Flask(__name__)  
+app.secret_key = ""    # python -c 'import secrets; print(secrets.token_hex())'
+                       
+
+
+
+
---
+++ flask_app/models/user.py
@@ -0,0 +1,68 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re  # the regex module
+
+
+
+class Users:
+    DB = 'login_registration'
+    def __init__(self, data):
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.email = data["email"]
+        self.password = data["password"]
+        self.created_at = data["created_at"].strftime("%B %drd %Y %H:%M:%S %p")
+        self.updated_at = data["updated_at"].strftime("%B %drd %Y %H:%M:%S %p")
+
+    
+
+    @classmethod
+    def create_user(cls, datas):
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(fname)s, %(lname)s, %(eml)s, %(psswrd)s);"
+        results = connectToMySQL(cls.DB).query_db(query, datas)
+        user_id_created = results
+        return user_id_created 
+
+    
+
+    # VALIDATE USER INFOS (REGISTER FORM)
+    @staticmethod
+    def validate_user_infos(data):
+        PASSWORD_REGEX = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$') 
+        #requires 8 chars, 1 upper, 1 lower, 1 number
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        is_valid = True
+        if len(data["fname"]) <= 0 or len(data["lname"]) <= 0 or len(data["eml"]) <= 0 or len(data["psswrd"]) <= 0:
+            is_valid = False
+            flash("All fields required", "register")
+            return is_valid
+        if len(data["fname"]) < 2:
+            is_valid = False
+            flash("First Name must be at least 2 characters", "register")
+        if len(data["lname"]) < 2:
+            is_valid = False
+            flash("Last Name must be at least 2 characters", "register")
+        if not EMAIL_REGEX.match(data['eml']):
+            flash("Invalid email address")
+            is_valid = False
+        if not PASSWORD_REGEX.match(data['psswrd']):
+            flash("requires 8 chars, 1 upper or lower, 1 number", "register")
+            is_valid = False    
+        if data["psswrd"] != data["conf_pass"]:
+            flash("Passwords don't match....you gotta fix that.", "register")    
+            is_valid = False    
+
+        return is_valid
+    
+
+    
+    # LOGIN
+    @classmethod
+    def user_with_specific_email(cls,data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL(cls.DB).query_db(query,data)
+        # Didn't find a matching user
+        if len(result) < 1:  
+            return False
+        return cls(result[0])
